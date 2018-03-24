e9f92c7c592c7a08decd25ef1678b4d8c872f22c
Marco Costa <mcosta@pminds.pt>
2018-03-24 22:13:50 +0000

    Handle rest response

--- src/main/scala/com/mabrcosta/keysmanager/users/key/rest/KeysHttpService.scala
+++ src/main/scala/com/mabrcosta/keysmanager/users/key/rest/KeysHttpService.scala
@@ -1,50 +1,53 @@
 package com.mabrcosta.keysmanager.users.key.rest
 
+import java.util.UUID
+
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import com.mabrcosta.keysmanager.users.key.business.api
-import com.mabrcosta.keysmanager.users.key.business.api.{KeysStack, KeysService}
-import com.mabrcosta.keysmanager.users.key.data.KeyData
+import com.mabrcosta.keysmanager.users.key.business.api.{KeysService, KeysStack}
+import com.mabrcosta.keysmanager.users.key.data.{Key, KeyData}
 import com.typesafe.scalalogging.LazyLogging
 import javax.inject.Inject
 import org.atnos.eff.FutureInterpretation._
-import org.atnos.eff.TimedFuture
 import org.atnos.eff.concurrent.Scheduler
 import org.atnos.eff.syntax.all._
+import org.atnos.eff.{Eff, TimedFuture}
 import slick.dbio.DBIO
 
-import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.ExecutionContext
 import scala.util.{Failure, Success}
 
 class KeysHttpService @Inject()(private val keyService: KeysService[DBIO, TimedFuture],
+                                implicit val executionContext: ExecutionContext,
                                 implicit val scheduler: Scheduler)
     extends KeyJsonSupport
     with LazyLogging {
 
   val routes: Route = pathPrefix("users" / JavaUUID / "keys") { uidOwner =>
     get {
-      onComplete(runAsync(keyService.getForOwner[KeysStack].runReader(uidOwner).runEither)) {
-        case Success(Right(keys)) => complete(keys)
-        case Success(Left(error)) => errorMapping(error)
-        case Failure(ex)          => complete(InternalServerError, ex.getMessage)
-      }
+      handleResponse[Seq[Key]](keyService.getForOwner[KeysStack], uidOwner, keys => complete(keys))
     } ~ post {
       entity(as[KeyData]) { key =>
-        onComplete(runAsync(keyService.addKey[KeysStack](key.value).runReader(uidOwner).runEither)) {
-          case Success(Right(res))  => complete(res)
-          case Success(Left(error)) => errorMapping(error)
-          case Failure(ex)          => complete(InternalServerError, ex.getMessage)
-        }
+        handleResponse[Key](keyService.addKey[KeysStack](key.value), uidOwner, key => complete(key))
       }
     } ~ path(JavaUUID) { uidKey =>
       delete {
-        onComplete(runAsync(keyService.deleteKey[KeysStack](uidKey).runReader(uidOwner).runEither)) {
-          case Success(Right(true))  => complete("")
-          case Success(Right(false)) => complete(InternalServerError)
-          case Success(Left(error))  => errorMapping(error)
-          case Failure(ex)           => complete(InternalServerError, ex.getMessage)
-        }
+        handleResponse[Boolean](keyService.deleteKey[KeysStack](uidKey),
+                                uidOwner,
+                                res => if (res) complete("") else complete(InternalServerError))
+      }
+    }
+  }
+
+  def handleResponse[T](effect: Eff[KeysStack, T], uidOwner: UUID, response: T => Route): Route = {
+    onComplete(runAsync(effect.runReader(uidOwner).runEither)) {
+      case Success(Right(res))  => response(res)
+      case Success(Left(error)) => errorMapping(error)
+      case Failure(ex)          => {
+        logger.error(ex.getMessage, ex)
+        complete(InternalServerError, ex.getMessage)
       }
     }
   }
